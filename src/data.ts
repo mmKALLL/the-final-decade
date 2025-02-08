927a4357ec5a2e85568437e2dd849a4500360628
Esa Koskinen <esakoskin2@gmail.com>
2025-02-08 20:39:42 +0900

    feat: refine the types to match the new game design better, add initial test data

--- src/data.ts
+++ src/data.ts
@@ -1,15 +1,76 @@
-import { GameState } from './types'
+import { generateContract, generateHuman, generateUpgrade } from './data-generators'
+import { Action, Contract, GameState, Human, Upgrade } from './types'
+
+const initialActions: Action[] = [
+  {
+    name: { 'en-US': 'Apply for funding', 'jp-JP': 'お金を募集する' },
+    turnCost: 3,
+    turnsInvested: 0,
+    effect: [{ paramEffected: 'money', apply: (value) => value + 10 }],
+  },
+  {
+    name: { 'en-US': 'Recruit a human', 'jp-JP': '人材を増やす' },
+    turnCost: 1,
+    turnsInvested: 0,
+    effect: [
+      { paramEffected: 'sp', apply: (value) => value - 10 },
+    ],
+    functionEffect: (gs) => ({ ...gs, humanSelections: [...gs.humanSelections, [generateHuman(), generateHuman(), generateHuman()]] }),
+  },
+  {
+    name: { 'en-US': 'Find a contract', 'jp-JP': '契約を探す' },
+    turnCost: 1,
+    turnsInvested: 0,
+    effect: [],
+    functionEffect: (gs) => ({ ...gs, contractSelections: [...gs.contractSelections, [generateContract(), generateContract(), generateContract()]] }),
+  },
+  {
+    name: { 'en-US': 'Research an upgrade', 'jp-JP': 'アップグレードを研究する' },
+    turnCost: 1,
+    turnsInvested: 0,
+    effect: [
+      { paramEffected: 'rp', apply: (value) => value - 10 },
+    ],
+    functionEffect: (gs) => ({ ...gs, upgradeSelections: [...gs.upgradeSelections, [generateUpgrade(), generateUpgrade(), generateUpgrade()]] }),
+  }, {
+    name: { 'en-US': 'Toggle language', 'jp-JP': '言語の切り替え' },
+    turnCost: 0,
+    turnsInvested: 0,
+    effect: [],
+    functionEffect: (gs) => ({ ...gs, language: gs.language === 'en-US' ? 'jp-JP' : 'en-US' }),
+  },
+]
+
+const bossContracts: Contract[] = [
+  {
+    name: { 'en-US': 'Boss Contract 1', 'jp-JP': 'ボス契約1' },
+    rarity: 'epic',
+    requirementDescription: { 'en-US': 'desc', 'jp-JP': 'desc' },
+    successDescription: { 'en-US': 'desc', 'jp-JP': 'desc' },
+    requirements: [{ paramEffected: 'rp', apply: (value) => value - 10 }],
+    onSuccess: [{ paramEffected: 'money', apply: (value) => value + 10 }],
+  },
+  {
+    name: { 'en-US': 'Boss Contract 2', 'jp-JP': 'ボス契約2' },
+    rarity: 'epic',
+    requirementDescription: { 'en-US': 'desc', 'jp-JP': 'desc' },
+    successDescription: { 'en-US': 'desc', 'jp-JP': 'desc' },
+    requirements: [{ paramEffected: 'rp', apply: (value) => value - 10 }],
+    onSuccess: [{ paramEffected: 'money', apply: (value) => value + 10 }],
+  }
+]
 
 export const initialGameState: GameState = {
   currentScreen: 'main',
+  language: 'en-US',
 
   turn: 0,
-  money: 0,
+  money: 100,
   passiveMoneyGain: 0,
-  asiOutcome: 0,
-  alignmentAcceptance: 0,
-  influence: 0,
-  trust: 0,
+  asiOutcome: 50,
+  alignmentAcceptance: -1,
+  influence: 100,
+  trust: 100,
 
   rp: 0,
   ep: 0,
@@ -23,7 +84,91 @@ export const initialGameState: GameState = {
   upgradeSelections: [],
   contractSelections: [],
 
-  availableActions: [],
+  bossContracts: bossContracts,
+  availableActions: initialActions,
+
   recentActions: [],
+
   recentGS: [],
 }
+
+// Humans for the game, in the style of Rimworld
+export const humans: Human[] = [
+  {
+    name: { 'en-US': 'John Doe', 'jp-JP': 'ジョン・ドウ' },
+    rarity: 'common',
+    wage: 10,
+    rank: 'junior',
+    spGeneration: 1,
+    epGeneration: 0,
+    rpGeneration: 0,
+  },
+  {
+    name: { 'en-US': 'Jane Doe', 'jp-JP': 'ジェーン・ドウ' },
+    rarity: 'common',
+    wage: 10,
+    rank: 'junior',
+    spGeneration: 0,
+    epGeneration: 1,
+    rpGeneration: 0,
+  },
+  {
+    name: { 'en-US': 'Alice', 'jp-JP': 'アリス' },
+    rarity: 'rare',
+    wage: 20,
+    rank: 'senior',
+    spGeneration: 1,
+    epGeneration: 2,
+    rpGeneration: 2,
+  },
+  {
+    name: { 'en-US': 'Bob', 'jp-JP': 'ボブ' },
+    rarity: 'rare',
+    wage: 20,
+    rank: 'senior',
+    spGeneration: 2,
+    epGeneration: 2,
+    rpGeneration: 2,
+  },
+  {
+    name: { 'en-US': 'Charlie', 'jp-JP': 'チャーリー' },
+    rarity: 'epic',
+    wage: 30,
+    rank: 'lead',
+    spGeneration: 3,
+    epGeneration: 3,
+    rpGeneration: 3,
+  },
+  {
+    name: { 'en-US': 'David', 'jp-JP': 'デイビッド' },
+    rarity: 'epic',
+    wage: 30,
+    rank: 'lead',
+    spGeneration: 3,
+    epGeneration: 3,
+    rpGeneration: 3,
+  },
+]
+
+export const contracts: Contract[] = [
+
+  {
+    name: { 'en-US': 'Contract 1', 'jp-JP': '契約1' },
+    rarity: 'common',
+    requirementDescription: { 'en-US': 'desc', 'jp-JP': 'desc' },
+    successDescription: { 'en-US': 'desc', 'jp-JP': 'desc' },
+    requirements: [{ paramEffected: 'rp', apply: (value) => value - 10 }],
+    onSuccess: [{ paramEffected: 'money', apply: (value) => value + 10 }],
+  }
+]
+
+export const upgrades: Upgrade[] = [
+  {
+    name: { 'en-US': 'Upgrade 1', 'jp-JP': 'アップグレード1' },
+    description: { 'en-US': 'desc', 'jp-JP': 'desc' },
+    rarity: 'common',
+    level: 0,
+    maxLevel: 1,
+    effect: [{ paramEffected: 'rp', apply: (value) => value + 10 }],
+  }
+]
--- src/types.ts
+++ src/types.ts
@@ -9,18 +9,21 @@ export type CurrentScreen =
   | 'humanAllocation' // unused
   | 'contracts'
 
+export type Language = 'en-US' | 'jp-JP'
+
 export type GameState = {
   currentScreen: 'main' | 'selection'
+  language: Language
 
   turn: number // Each unit of time represents one month
 
-  // 0-100. Public view of alignment. One of the win/loss conditions, influences what rate of org breakthroughs lead to alignment improvements. Second-order effect on asiOutcome.
+  // -5 - 5. Public view of alignment. ASI outcome is shifted by this amount each turn. Decreases by 1 every year. Originally one of the win/loss conditions, influences what rate of org breakthroughs lead to alignment improvements. Second-order effect on asiOutcome.
   alignmentAcceptance: number
 
   // 0-100. Shifted whenever breakthroughs are made, by the level of the feature receiving the breakthrough. 0: capabilities win; misaligned ASI. 100: aligned ASI.
   asiOutcome: number
 
-  // 0-200. Trust towards your organization. Gain or lose depending on how your fund/contract money is handled. If you have high trust you'll get better contracts
+  // 0-200. Trust towards your organization. Gain or lose depending on how your fund/contract money is handled. If you have high trust you'll get better contracts and recruits
   trust: number
 
   // 0-200. Influence is a percentage multiplier to the effect of your social actions (like increasing alignment acceptance)
@@ -41,12 +44,15 @@ export type GameState = {
   contracts: Contract[]
   upgrades: Upgrade[]
 
-  humanSelections: []
-  upgradeSelections: []
-  contractSelections: []
+  humanSelections: Human[][]
+  upgradeSelections: Upgrade[][]
+  contractSelections: Contract[][]
+
+  bossContracts: Contract[]
 
   availableActions: Action[]
   recentActions: Action[]
+
   recentGS: GameState[]
 
   // Organization playerOrganization: Organization('Meta AI', -30, FeatureName.automation);
@@ -72,22 +78,27 @@ export type GameState = {
   // organizationFunctionModifiers: Record<Param, CurriedModifier[]>
 }
 
+export type Rarity = 'common' | 'uncommon' | 'rare' | 'epic'
+
 export type Human = {
-  name: string
+  name: Record<Language, string>
   // description: string
+  rarity: Rarity
   wage: number
+  rank: 'volunteer' | 'junior' | 'intermediate' | 'senior' | 'lead' // seniors and leads multiply the output of others, but at high cost
   spGeneration: number
   epGeneration: number
   rpGeneration: number
-  // specialEffects: Effect[]
+  // specialEffects?: Effect
 }
 
 export type Upgrade = {
-  name: string
-  description: string
+  name: Record<Language, string>
+  description: Record<Language, string>
+  rarity: Rarity
   level: number
   maxLevel: number
-  effects: Effect[]
+  effect: Effect
 
   // List<Modifier> modifiers = []; // mods that affect resource gain
   // List<ParamEventHandler> paramEventHandlers = []; // handlers that can stack more effects when a parameter's value has been changed
@@ -111,35 +122,38 @@ export type Param =
   | 'rp'
   | 'ep'
   | 'sp'
-  | 'humanSelection'
-  | 'upgradeSelection'
-  | 'contractSelection'
 
 export type Contract = {
-  name: string
-  acceptDescription: string
-  successDescription: string
-  failureDescription: string
-  requirementDescription: string
-  onAccept: Effect[]
-  onSuccess: Effect[]
+  name: Record<Language, string>
+  rarity: Rarity
+  // acceptDescription: Record<Language, string>
+  successDescription: Record<Language, string>
+  // failureDescription: Record<Language, string>
+  requirementDescription: Record<Language, string>
+  // onAccept: Effect
+  onSuccess: Effect
   // onFailure: Effect
-  requirements: Effect[]
+  requirements: Effect
 }
 
 export type Action = {
-  name: string
-  description: string
-  turnsInvested: number
+  name: Record<Language, string>
+  description?: Record<Language, string>
+  enabledCondition?: (gs: GameState) => boolean
   turnCost: number
-  effects: Effect[]
+  turnsInvested: number
+  effect: Effect
   functionEffect?: (gs: GameState) => GameState
 }
 
-export type Effect = {
+export type SingleEffect = {
+  source?: string
+  condition?: (gs: GameState, paramEffected: Param, baseAmount: number) => boolean
   paramEffected: Param
   apply: (value: number) => number
 }
+
+export type Effect = SingleEffect[]
 
 // const initialOrganizations = [
 //   Organization('Meta AI', -20, FeatureName.automation, 0),
