8ee17f1179248e8858a1b646bd23d1898d57e983
Esa Koskinen <esakoskin2@gmail.com>
2025-04-03 02:15:48 +0900

    feat: fade out actions that are unavailable

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,19 +1,23 @@
 import { generateActionDescription } from '../data/data-generators'
-import { useGameState } from '../gamestate-hooks'
+import { canApplyAction, useGameState } from '../gamestate-hooks'
 import { Action } from '../types'
 
 export function Button({ action }: { action: Action }) {
   const { gs, dispatch } = useGameState()
   const description = action.description?.[gs.language] || generateActionDescription(action)
+  const disabled = !canApplyAction(gs, action)
 
   return (
-    <div className="button">
-      <button onClick={() => dispatch(action)} title={description || undefined}>
-        <div className="action-content">
-          <div className="action-name">{action.name[gs.language]}</div>
-          {description && <div className="action-description">{description}</div>}
-        </div>
-      </button>
-    </div>
+    <button
+      disabled={disabled}
+      className={`button ${disabled ? 'faded' : ''}`}
+      onClick={() => dispatch(action)}
+      title={description || undefined}
+    >
+      <div className="action-content">
+        <div className="action-name">{action.name[gs.language]}</div>
+        {description && <div className="action-description">{description}</div>}
+      </div>
+    </button>
   )
 }
