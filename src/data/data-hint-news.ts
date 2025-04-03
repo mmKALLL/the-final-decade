a9377b9b7533cb5b975f72ece77cc5b27fee0b89
Esa Koskinen <esakoskin2@gmail.com>
2025-04-04 02:35:49 +0900

    feat: add scaffolding for some kind of news scroll data

---
+++ src/data/data-hint-news.ts
@@ -0,0 +1,25 @@
+export const hintNews = [
+  'You get what you measure.',
+
+  'A time traveling superintelligence has not visited us yet. Does this mean either time travel or superintelligence is not possible? Or is it simply waiting for us to invent itself first?',
+
+  'AI system claimed to find room-temperature superconductors. Experts furiously trying to repeat the experiment.',
+
+  'Aligning things is also hard in CSS.',
+
+  'Mineralis - 30 years of life advice in 3 hours of gameplay',
+
+  'AI business strategists decimated humans in competition.',
+
+  'Is an apocalypse coming or just another aspect of overblown hype? Reasonable people with deep expertise are on both sides of the argument, but most are calling for increased caution.',
+
+  'Better safe than sorry.',
+
+  // AI generated
+  "AI finally learns sarcasm. Now no one can tell if it's serious—or sentient.",
+  'AI researcher claims to have solved alignment. Human researchers have no way to verify its proof.',
+  'Scientists report mysterious disappearance of edge cases in latest dataset. Investigation ongoing.',
+  'All language models hallucinate, but some are unintentionally hilarious.',
+  'To err is human. To optimize is AI.',
+  'Language models hallucinate. Some even charge you for it.',
+]
